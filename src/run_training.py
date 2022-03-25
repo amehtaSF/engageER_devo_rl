e82b8821a3f76a26d00a4d0e70e34b99de7ec428
JTPetter <61797391+JTPetter@users.noreply.github.com>
2022-03-25 12:13:58 -0700

    Change simulation setup and analysis script to be more user-friendly

--- src/run_training.py
+++ src/run_training.py
@@ -1,108 +1,163 @@
+import os
+
 import numpy as np
 import random
 import matplotlib.pyplot as plt
 import pandas as pd
 import path
+import os
 
 from environment import Stimulus, AgentStatus, EmotionEnv
 from agent import QTableAgent
 
-random.seed(123)
-np.random.seed(123)
+simulationValues = [50, 100, 250, 500, 1000, 2000, 4000, 8000, 15000]  # a vector of values that the parameter you want to change should take.
+                            # For no simulations, set to [1] and set all paramter values yourself
+
+file_name = "N_STIMULI"      # the first part of the file name, automatically appended with the respective simulation value and data description
+# DONT USE NUMBERS IN FILE NAME
+
+folder_path = "../datasets/" + file_name   # where to save the data
+os.makedirs(folder_path)     # create a folder
+
+for sv in simulationValues:
 
 
-N_RUNS = 100
-N_STIMULI = 2000
+    # parameter list; to run simulations, change the desired parameter to "sv"
+    N_RUNS = 10000
+    N_STIMULI = sv
+    N_ACTIONS = 3
 
-engage_delay = 2
-engage_benefit = 4
-disengage_benefit = 5
-engage_adaptation = 2
+    alpha = .001
+    gamma = .99
+    epsilon = .1
 
-# p_recurrence does not do anything at the moment
-stimuli_list = [random.choice(
-    [Stimulus(id=i, emo_intensity=8, p_recurrence=1),
-     Stimulus(id=i, emo_intensity=9, p_recurrence=.5),
-     Stimulus(id=i, emo_intensity=10, p_recurrence=1),
-     Stimulus(id=i, emo_intensity=7, p_recurrence=.8)]
-) for i in range(N_STIMULI)]
+    engage_delay = 2
+    engage_benefit = 4
+    disengage_benefit = 5
+    engage_adaptation = 2
+    seed = 123
 
-agent_status = AgentStatus()
+    random.seed(seed)
+    np.random.seed(seed)
 
-env = EmotionEnv(engage_delay=engage_delay,
-                 engage_benefit=engage_benefit,
-                 disengage_benefit=disengage_benefit,
-                 engage_adaptation=engage_adaptation,
-                 stimuli=stimuli_list,
-                 agent_status=agent_status
-                 )
-env.reset()
-n_actions = 3
-alpha = .001
-gamma = .99
-epsilon = .1
+    # p_recurrence does not do anything at the moment
+    stimuli_list = [random.choice(
+        [Stimulus(id=i, emo_intensity=8, p_recurrence=1),
+         Stimulus(id=i, emo_intensity=9, p_recurrence=.5),
+         Stimulus(id=i, emo_intensity=10, p_recurrence=1),
+         Stimulus(id=i, emo_intensity=7, p_recurrence=.8)]
+    ) for i in range(N_STIMULI)]
 
+    agent_status = AgentStatus()
 
-agent = QTableAgent(env.n_stimuli, n_actions=n_actions, alpha=alpha, gamma=gamma, epsilon=epsilon)
-
-state = agent_status.current_id
-action = agent.choose_action(state, policy="epsilon_greedy")
-
-# Record actions and rewards
-action_counts = np.zeros((N_RUNS, agent.n_actions))
-reward_counts = np.zeros((N_RUNS, agent.n_actions))
-zero_counts = np.zeros(N_RUNS)
-
-# Run simulation
-for i in range(N_RUNS):
-    #env.render()
-    next_state, reward, done, info = env.step(action)
-    agent.update(state, next_state, action, reward)
-    print(f'action: {action}, reward: {reward}, step: {i}')
-    env.render()
-    action_counts[i, action] += 1
-    reward_counts[i, action] += reward
-    if done:
-        env.reset()
-        action = agent.choose_action(state, policy="epsilon_greedy")
-
-
-# Plot choices
-
-time = np.arange(0, N_RUNS)
-action_cumsum = np.cumsum(action_counts, axis=0)
-plt.plot(time, action_cumsum[:, 0], marker='', color='olive', linewidth=2, label='inaction')
-plt.plot(time, action_cumsum[:, 1], marker='', color='blue', linewidth=2, label='disengage')
-plt.plot(time, action_cumsum[:, 2], marker='', color='red', linewidth=2, label='engage')
-plt.legend()
-plt.show()
-
-
-# plot rewards
-
-inaction_timeline = np.cumsum(reward_counts[:, 0] != 0) + 1
-disengage_timeline = np.cumsum(reward_counts[:, 1] != 0) + 1
-engage_timeline = np.cumsum(reward_counts[:, 2] != 0) + 1
-reward_cumsum = np.cumsum(reward_counts, axis=0)
-reward_cumsum[:, 0] / np.arange(1, N_RUNS + 1, 1)
-plt.plot(time, reward_cumsum[:, 0]/inaction_timeline, marker='', color='olive', linewidth=2, label='inaction')
-plt.plot(time, reward_cumsum[:, 1]/disengage_timeline, marker='', color='blue', linewidth=2, label='disengage')
-plt.plot(time, reward_cumsum[:, 2]/engage_timeline, marker='', color='red', linewidth=2, label='engage')
-plt.legend()
-plt.show()
+    env = EmotionEnv(engage_delay=engage_delay,
+                     engage_benefit=engage_benefit,
+                     disengage_benefit=disengage_benefit,
+                     engage_adaptation=engage_adaptation,
+                     stimuli=stimuli_list,
+                     agent_status=agent_status
+                     )
+    env.reset()
 
 
 
-#set options for pandas
-# pd.set_option('display.max_columns', None)
-# pd.set_option('display.width', None)
-# pd.set_option('display.max_colwidth', None)
-#
-# df1 = pd.DataFrame({'inaction': action_cumsum[:, 0], 'disengange': action_cumsum[:, 1], 'engange': action_cumsum[:, 2]})
-# file_name1='forced_engage_actions' + ".csv"  #+ str(seed)
-# df1.to_csv(file_name1)
-#
-# df2 = pd.DataFrame({'inaction': reward_cumsum[:, 0]/inaction_timeline, 'disengange': reward_cumsum[:, 1]/disengage_timeline,
-#                     'engange': reward_cumsum[:, 2]/engage_timeline})
-# file_name2='forced_engage_rewards' + ".csv"  #+ str(seed)
-# df2.to_csv(file_name2)
+    agent = QTableAgent(env.n_stimuli, n_actions=N_ACTIONS, alpha=alpha, gamma=gamma, epsilon=epsilon)
+
+    state = agent_status.current_id
+    action = agent.choose_action(state, policy="epsilon_greedy")
+
+    # Record actions and rewards
+    action_counts = np.zeros((N_RUNS, agent.n_actions))
+    reward_counts = np.zeros((N_RUNS, agent.n_actions))
+
+    # Run simulation
+    for i in range(N_RUNS):
+        #env.render()
+        next_state, reward, done, info = env.step(action)
+        agent.update(state, next_state, action, reward)
+        print(f'action: {action}, reward: {reward}, step: {i}')
+        env.render()
+        action_counts[i, action] += 1
+        reward_counts[i, action] += reward
+        if done:
+            env.reset()
+            action = agent.choose_action(state, policy="epsilon_greedy")
+
+
+
+    #create data for running the same stimulus 3 times with every action
+    intensity_values = np.zeros((9, N_ACTIONS))
+    stimuli_list2 = [Stimulus(id=N_STIMULI + 1, emo_intensity=8, p_recurrence=5)]
+    agent_status2 = AgentStatus()
+    env2 = EmotionEnv(engage_delay=engage_delay,
+                      engage_benefit=engage_benefit,
+                      disengage_benefit=disengage_benefit,
+                      engage_adaptation=engage_adaptation,
+                      stimuli=stimuli_list2,
+                      agent_status=agent_status2
+                      )
+    env2.reset()
+    state = agent_status2.current_id
+    for ac in [0, 1, 2]:
+        action = ac
+        for i in np.arange(0, 9, 1):
+            next_state, reward, done, info = env2.step(action)
+            print(f'action: {action}, reward: {reward}, step: {i}')
+            intensity_values[i, ac] = agent_status2.current_emo_intensity
+            env2.render()
+            if done:
+                env2.reset()
+
+
+    #plot the emotional intensity curve per action
+    time = np.arange(0, 9)
+    plt.plot(time, intensity_values[:, 0], marker='', color='olive', linewidth=2, label='inaction')
+    plt.plot(time, intensity_values[:, 1], marker='', color='blue', linewidth=2, label='disengage')
+    plt.plot(time, intensity_values[:, 2], marker='', color='red', linewidth=2, label='engage')
+    plt.legend()
+    #plt.show()
+
+    # Plot choices
+    time = np.arange(0, N_RUNS)
+    action_cumsum = np.cumsum(action_counts, axis=0)
+    plt.plot(time, action_cumsum[:, 0], marker='', color='olive', linewidth=2, label='inaction')
+    plt.plot(time, action_cumsum[:, 1], marker='', color='blue', linewidth=2, label='disengage')
+    plt.plot(time, action_cumsum[:, 2], marker='', color='red', linewidth=2, label='engage')
+    plt.legend()
+    #plt.show()
+
+
+    # plot rewards
+    inaction_timeline = np.cumsum(reward_counts[:, 0] != 0) + 1
+    disengage_timeline = np.cumsum(reward_counts[:, 1] != 0) + 1
+    engage_timeline = np.cumsum(reward_counts[:, 2] != 0) + 1
+    reward_cumsum = np.cumsum(reward_counts, axis=0)
+    reward_cumsum[:, 0] / np.arange(1, N_RUNS + 1, 1)
+    plt.plot(time, reward_cumsum[:, 0]/inaction_timeline, marker='', color='olive', linewidth=2, label='inaction')
+    plt.plot(time, reward_cumsum[:, 1]/disengage_timeline, marker='', color='blue', linewidth=2, label='disengage')
+    plt.plot(time, reward_cumsum[:, 2]/engage_timeline, marker='', color='red', linewidth=2, label='engage')
+    plt.legend()
+    #plt.show()
+
+    #set options for pandas
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', None)
+
+    #to write the actions to csv
+    df1 = pd.DataFrame({'inaction': action_cumsum[:, 0], 'disengage': action_cumsum[:, 1], 'engage': action_cumsum[:, 2]})
+    file_name1 = folder_path + '/' + file_name + '_' + str(sv) + '_actionCumSum' '.csv'
+    df1.to_csv(file_name1)
+
+
+    #To write the rewards to csv
+    df2 = pd.DataFrame({'inaction': reward_cumsum[:, 0]/inaction_timeline, 'disengage': reward_cumsum[:, 1]/disengage_timeline,
+                        'engage': reward_cumsum[:, 2]/engage_timeline})
+    file_name2 = folder_path + '/' + file_name + '_' + str(sv) + '_RewardsCumMean' '.csv'
+    df2.to_csv(file_name2)
+
+
+    #To write action trajectory to csv
+    df3 = pd.DataFrame({'inaction': intensity_values[:, 0], 'disengage': intensity_values[:, 1], 'engage': intensity_values[:, 2]})
+    file_name3 = folder_path + '/' + file_name + '_' + str(sv) + '_actionTrajectory' '.csv'
+    df3.to_csv(file_name3)
